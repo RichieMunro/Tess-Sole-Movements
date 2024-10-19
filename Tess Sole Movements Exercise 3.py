f757ae1a02362bfcad19b5e7299d87f7c3661206
RichieMunro <richie.munro@outlook.com>
2024-10-19 22:51:28 +1000

    Add files via upload
    A small program about Tess movements with only one direction stated, tess.left(3645)

---
+++ Tess Sole Movements Exercise 3.py
@@ -0,0 +1,12 @@
+import turtle
+
+window = turtle.Screen() #set up the window required to run the turtle program
+window.bgcolor ("light green") #back ground color that is chosen 
+tess = turtle.Turtle()
+tess.color = ("blue")
+tess.pensize = ("5")
+
+tess.back(0)
+tess.left(3645)
+
+wn.mainloop()
